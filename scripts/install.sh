2a734c7c8938339629037a02e71c2b6c3ab1d2b2
Christopher Agocs <chris@agocs.org>
2022-03-27 22:59:38 -0400

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 echo "copying templates to ~/.groceries"
 mkdir ~/.groceries
-cp templates ~/.groceries
+cp -r templates ~/.groceries
 
 echo "copying `groceries` command to /usr/local/bin"
-cp groceries.sh /usr/local/bin/groceries
+cp scripts/groceries.sh /usr/local/bin/groceries
 
 echo "all done!"
